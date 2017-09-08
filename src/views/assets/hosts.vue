9dfe10287ce7872ba143c29007afa6a94a32e7e5
itimor <itimor@techdog.com>
2017-09-08 17:30:14 +0800

    div圆角化

--- src/views/assets/hosts.vue
+++ src/views/assets/hosts.vue
@@ -1,72 +1,76 @@
 <template>
     <div class="body-main">
-            <div class="head-lavel">
-                <div class="table-button">
-                    <el-button type="info" icon="plus" @click="handleCreate">新建项目</el-button>
-                    <div class="checkbox">
+        <div class="head-lavel">
+            <div class="table-button">
+                <el-button type="info" icon="plus" @click="handleCreate">新建项目</el-button>
+                <div class="checkbox">
                     <el-checkbox v-model="checkList.expiredate">租期</el-checkbox>
                     <el-checkbox v-model="checkList.staff">项目负责人</el-checkbox>
                     <el-checkbox v-model="checkList.sn">SN</el-checkbox>
-                    </div>
-                </div>
-                <div class="table-search">
-                    <el-input
-                            placeholder="搜索 ..."
-                            icon="search"
-                            v-model="searchdata"
-                            @keyup.enter.native="searchClick"
-                            :on-icon-click="searchClick">
-                    </el-input>
                 </div>
             </div>
-            <div>
-                <el-table :data="tableData" border style="width: 100%">
-                    <el-table-column prop='name' label='主机名' sortable>
-                        <template scope="scope">
-                            <div slot="reference" class="name-wrapper" style="text-align: center">
-                                <el-button type="text" @click="handleEdit(scope.row)">{{ scope.row.name }}
-
-                                </el-button>
-                            </div>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop='publicip' label='外网ip'></el-table-column>
-                    <el-table-column prop='privateip' label='内网ip'></el-table-column>
-                    <el-table-column prop='idc' label='机房' sortable>
-                        <template scope="scope">
-                            <div slot="reference" class="name-wrapper" style="text-align: center">
-                                {{scope.row.idc.name}}
-
-                            </div>
-                        </template>
-                    </el-table-column>
-                    <el-table-column v-if="checkList.sn" prop='sn' label='sn'></el-table-column>
-                    <el-table-column prop='cpu' label='cpu'></el-table-column>
-                    <el-table-column prop='memory' label='内存(M)' sortable></el-table-column>
-                    <el-table-column prop='disktype' label='磁盘类型'></el-table-column>
-                    <el-table-column prop='diskcapacity' label='磁盘大小(G)'></el-table-column>
-                    <el-table-column v-if="checkList.expiredate" prop='expiredate' label='租约到期时间' sortable></el-table-column>
-                    <el-table-column v-if="checkList.staff" prop='staff' label='项目负责人'>
-                        <template scope="scope">
-                            <div slot="reference" class="name-wrapper" style="text-align: center">
-                                {{scope.row.staff.name}}
-                            </div>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop='comment' label='备注'></el-table-column>
-                </el-table>
+            <div class="table-search">
+                <el-input
+                        placeholder="搜索 ..."
+                        icon="search"
+                        v-model="searchdata"
+                        @keyup.enter.native="searchClick"
+                        :on-icon-click="searchClick">
+                </el-input>
             </div>
-            <div class="table-pagination">
-                <el-pagination
-                        @size-change="handleSizeChange"
-                        @current-change="handleCurrentChange"
-                        :current-page.sync="currentPage"
-                        :page-sizes="pagesize"
-                        :page-size="limit"
-                        layout="prev, pager, next, sizes"
-                        :total="tabletotal">
-                </el-pagination>
-            </div>
+        </div>
+        <div>
+            <el-table :data="tableData" border style="width: 100%">
+                <el-table-column prop='name' label='主机名' sortable>
+                    <template scope="scope">
+                        <div slot="reference" class="name-wrapper" style="text-align: center">
+                            <el-button type="text" @click="handleEdit(scope.row)">{{ scope.row.name }}
+
+
+                            </el-button>
+                        </div>
+                    </template>
+                </el-table-column>
+                <el-table-column prop='publicip' label='外网ip'></el-table-column>
+                <el-table-column prop='privateip' label='内网ip'></el-table-column>
+                <el-table-column prop='idc' label='机房' sortable>
+                    <template scope="scope">
+                        <div slot="reference" class="name-wrapper" style="text-align: center">
+                            {{scope.row.idc.name}}
+
+
+                        </div>
+                    </template>
+                </el-table-column>
+                <el-table-column v-if="checkList.sn" prop='sn' label='sn'></el-table-column>
+                <el-table-column prop='cpu' label='cpu'></el-table-column>
+                <el-table-column prop='memory' label='内存(M)' sortable></el-table-column>
+                <el-table-column prop='disktype' label='磁盘类型'></el-table-column>
+                <el-table-column prop='diskcapacity' label='磁盘大小(G)'></el-table-column>
+                <el-table-column v-if="checkList.expiredate" prop='expiredate' label='租约到期时间'
+                                 sortable></el-table-column>
+                <el-table-column v-if="checkList.staff" prop='staff' label='项目负责人'>
+                    <template scope="scope">
+                        <div slot="reference" class="name-wrapper" style="text-align: center">
+                            {{scope.row.staff.name}}
+
+                        </div>
+                    </template>
+                </el-table-column>
+                <el-table-column prop='comment' label='备注'></el-table-column>
+            </el-table>
+        </div>
+        <div class="table-pagination">
+            <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page.sync="currentPage"
+                    :page-sizes="pagesize"
+                    :page-size="limit"
+                    layout="prev, pager, next, sizes"
+                    :total="tabletotal">
+            </el-pagination>
+        </div>
 
         <el-dialog title="添加新主机" :visible.sync="addForm" size="small">
             <add-host @DialogStatus="getDialogStatus"></add-host>
