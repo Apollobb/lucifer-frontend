1560ea7aacf4261c0bef55af43c4ae3c92fc6691
itimor <itimor@techdog.com>
2017-09-21 17:51:22 +0800

    change login

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -31,9 +31,6 @@
 <script>
     import {Login} from "@/api/auth"
     import {mapState, mapActions} from 'vuex'
-    import Stars from '../../../static/js/Star'
-    import Moon from '../../../static/js/Moon'
-    import Meteor   from '../../../static/js/Meteor'
 
     export default {
         components: {},
@@ -59,51 +56,6 @@
             }
         },
 
-        mounted() {
-            let canvas = document.getElementById('canvas'),
-                ctx = canvas.getContext('2d'),
-                width = window.innerWidth,
-                height = window.innerHeight,
-                //实例化月亮和星星。流星是随机时间生成，所以只初始化数组
-                moon = new Moon(ctx, width, height),
-                stars = new Stars(ctx, width, height, 200),
-                meteors = [],
-                count = 0
-
-            canvas.width = width;
-            canvas.height = height;
-
-            const meteorGenerator = () => {
-                //x位置偏移，以免经过月亮
-                let x = Math.random() * width + 800
-                meteors.push(new Meteor(ctx, x, height))
-
-                //每隔随机时间，生成新流星
-                setTimeout(() => {
-                    meteorGenerator()
-
-                }, Math.random() * 2000)
-            }
-
-            const frame = () => {
-                count++
-                count % 10 == 0 && stars.blink()
-                moon.draw()
-                stars.draw()
-
-                meteors.forEach((meteor, index, arr) => {
-                    //如果流星离开视野之内，销毁流星实例，回收内存
-                    if (meteor.flow()) {
-                        meteor.draw()
-                    } else {
-                        arr.splice(index, 1)
-                    }
-                })
-                requestAnimationFrame(frame)
-            }
-            meteorGenerator()
-            frame()
-        },
         methods: {
             ...mapActions(['Login']),
             handleLogin() {
