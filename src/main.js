4ab3e5794211f62a9cd7d1808c16f241b830017e
itimor <itimor@techdog.com>
2017-09-25 14:51:00 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -19,10 +19,6 @@ import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 Vue.component('icon', Icon);
 
-/* table */
-import DataTables from 'vue-data-tables'
-Vue.use(DataTables);
-
 /* markdown */
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
@@ -40,11 +36,6 @@ Object.keys(filters).forEach(key => {
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
-
-/* 自动滚动到底部 */
-import VueChatScroll from 'vue-chat-scroll'
-Vue.use(VueChatScroll);
-
 
 // 生产环境错误日志
 if (process.env === 'production') {
